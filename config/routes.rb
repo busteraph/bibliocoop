03714a85e13735ade87ec158c3cec10f8e1b0fc6
raphook <raphael69libert@gmail.com>
2015-04-27 14:22:46 +0000

    CRUD Livre ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'livres#index'
+
+  resources :livres
+
   devise_for :users
   
   # The priority is based upon order of creation: first created -> highest priority.
